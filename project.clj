ce5f34f4f160c135ff61f893bbcf769be9825f7d
Doug Knight <doug.knight@livingsocial.com>
2014-12-12 08:36:25 -0600

    added datomic dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [
+                 [org.clojure/clojure "1.6.0"]
+                 [com.datomic/datomic-free "0.9.5067" :exclusions [joda-time]]]
   :main ^:skip-aot clojure-money.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
