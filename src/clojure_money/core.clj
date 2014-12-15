1e998a3d7b1a177c088d952cbafcc2b5907acfba
Doug Knight <dougbknight@gmail.com>
2014-12-15 13:07:05 -0600

    got one unit test working, add and retrieve an accout

--- src/clojure_money/core.clj
+++ src/clojure_money/core.clj
@@ -1,5 +1,38 @@
 (ns clojure-money.core
+  (:require [datomic.api :as d :refer :all])
   (:gen-class))
+
+;TODO Move this to a separate file
+(def schema [{:db/id (d/tempid :db.part/db)
+              :db/ident :account/name
+              :db/valueType :db.type/string
+              :db/cardinality :db.cardinality/one
+              :db/doc "The name of the account"
+              :db.install/_attribute :db.part/db}])
+
+(def conn nil)
+
+(defn add-account
+  "Saves an account to the database"
+  [account-name]
+  (let [new-id (d/tempid :db.part/user)]
+    @(d/transact
+      conn
+      [{:db/id new-id
+        :account/name account-name}])))
+
+(defn all-accounts
+  "Returns all of the accounts in the system"
+  []
+  (d/q
+    '[:find ?a
+      :where [_ :account/name ?a]]
+    (d/db conn)))
+
+(defn find-account-by-path
+  "Finds an account with the specified path"
+  [path]
+  (d/pull (d/db conn) '[*] [:account/name path]))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/clojure_money/core_test.clj
+++ test/clojure_money/core_test.clj
@@ -1,7 +1,21 @@
 (ns clojure-money.core-test
   (:require [clojure.test :refer :all]
-            [clojure-money.core :refer :all]))
+            [clojure-money.core :refer :all]
+            [datomic.api :as d :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;TODO Need to figoure out how to handle different URIs for different environments
+(def uri "datomic:mem://money")
+
+(defn create-empty-db []
+  (d/delete-database uri)
+  (d/create-database uri)
+  (let [conn (d/connect uri)]
+    (d/transact conn schema)
+    conn))
+
+(deftest a-saved-account-can-be-retrieved
+  (with-redefs [conn (create-empty-db)]
+    (testing "After I save an account, I can retrieve it"
+      (add-account "Checking")
+      (printf "all-accounts returns %s\n" (all-accounts))
+      (is (= [["Checking"]] (all-accounts))))))
