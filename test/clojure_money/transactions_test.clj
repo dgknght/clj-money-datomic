945bec4c5d762ff4cf788e0ae0258fe22f7ff3ce
Doug Knight <dougbknight@gmail.com>
2014-12-17 13:44:41 -0600

    came to a working solution for testing transaction write/read

--- test/clojure_money/transactions_test.clj
+++ test/clojure_money/transactions_test.clj
@@ -6,16 +6,19 @@
             [clojure-money.transactions :refer :all]))
 
 ;; When I add a transaction, it appears in the list of transactions
-(expect #{[(str->date-time "2014-12-15") "Paycheck" [#{:transaction-item.action/debit "Checking" 1000} #{:transaction-item.action/credit "Salary" 1000}]]}
+(expect (more-> (str->date-time "2014-12-15") :transaction/date
+                "Paycheck" :transaction/description
+                (BigDecimal. 1000) (-> :transaction/items first :transaction-item/amount)
+                :transaction-item.action/debit (-> :transaction/items first :transaction-item/action))
         (with-redefs [conn (create-empty-db)]
           (do
-              (add-account "Checking" :account.type/asset)
-              (add-account "Salary" :account.type/income)
-              (add-transaction (str->date-time "2014-12-15")
-                               "Paycheck"
-                               [[:transaction-item.action/debit "Checking" (BigDecimal. 1000)]
-                                [:transaction-item.action/credit "Salary" (BigDecimal. 1000)]])
-              (get-transactions (str->date-time "2014-12-01") (str->date-time "2014-12-31")))))
+            (add-account "Checking" :account.type/asset)
+            (add-account "Salary" :account.type/income)
+            (add-transaction (str->date-time "2014-12-15")
+                             "Paycheck"
+                             [[:transaction-item.action/debit "Checking" (BigDecimal. 1000)]
+                              [:transaction-item.action/credit "Salary" (BigDecimal. 1000)]])
+            (first (get-transactions (str->date-time "2014-12-01") (str->date-time "2014-12-31"))))))
 
 ;; When I add a transaction, it should affect the balance of the referenced accounts
 
