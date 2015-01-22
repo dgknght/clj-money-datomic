5d12d8a4b7a46a5c6102edc105ec01dab83cd7ce
Doug Knight <dougbknight@gmail.com>
2015-01-22 17:01:22 -0600

    working the balance sheet report tests

--- src/clojure_money/reports.clj
+++ src/clojure_money/reports.clj
@@ -1,10 +1,29 @@
 (ns clojure-money.reports
-  (:require [datomic.api :as d :refer [q]])
+  (:use clojure-money.accounts)
   (:gen-class))
 
+;; This class shouldn't reference any database functions directly
+;; Rather, it should compose HOF from accounts and transactions
+;; to create the reports
+
+(declare interleave-summaries)
 (defn balance-sheet-report
   "Returns a balance sheet report"
   [db as-of-date]
-  (d/q  '[:find ?name
-          :where [_ :account/name ?name]]
-       db))
+  (->> (all-accounts db)
+       (sort-by :account/type)
+       (partition-by :account/type)
+       (interleave-summaries)))
+
+(defn interleave-summaries
+  "Takes a list of accounts grouped by account type interleaves account tyep summaries"
+  [grouped-accounts]
+  (reduce (fn [result group]
+            (let [total (reduce (fn [total account] (+ total (:account/balance account))) 0 group)]
+              (concat (conj result {:caption (:account/type (first group))
+                                  :value total
+                                  :style :header
+                                  :depth 0})
+                    group)))
+          []
+          grouped-accounts))
