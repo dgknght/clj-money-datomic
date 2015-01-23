b230f8653682342315952b24dab1c16dd034870c
Doug Knight <dougbknight@gmail.com>
2015-01-23 11:04:24 -0600

    fleshing out the balance sheet report function

--- src/clojure_money/reports.clj
+++ src/clojure_money/reports.clj
@@ -1,5 +1,6 @@
 (ns clojure-money.reports
-  (:use clojure-money.accounts)
+  (:use clojure-money.accounts
+        clojure.set)
   (:gen-class))
 
 ;; This class shouldn't reference any database functions directly
@@ -15,15 +16,41 @@
        (partition-by :account/type)
        (interleave-summaries)))
 
+(defn entity-map->hash-map
+  [entity]
+  (apply hash-map (-> entity
+                      seq
+                      flatten)))
+
+(defn format-account
+  "Takes an entity map of an account and formats it for a report"
+  [account]
+  (-> account
+      entity-map->hash-map
+      (rename-keys {:account/name :caption :account/balance :value})
+      (select-keys [:caption :value])
+      (assoc :depth 0 :style :data)))
+
+(defn format-accounts
+  "Takes a list of accounts and formats them for a report"
+  [accounts]
+  (map format-account accounts))
+
+(def account-type-caption-map {:account.type/asset "Assets"
+                               :account.type/liability "Liabilities"
+                               :account.type/equity "Equity"
+                               :account.type/income "Income"
+                               :account.type/expense "Expense"})
 (defn interleave-summaries
-  "Takes a list of accounts grouped by account type interleaves account tyep summaries"
+  "Takes a list of accounts grouped by account type interleaves account type summaries"
   [grouped-accounts]
   (reduce (fn [result group]
-            (let [total (reduce (fn [total account] (+ total (:account/balance account))) 0 group)]
-              (concat (conj result {:caption (:account/type (first group))
-                                  :value total
-                                  :style :header
-                                  :depth 0})
-                    group)))
+            (let [total (reduce (fn [total account] (+ total (:account/balance account))) 0 group)
+                  header {:caption ((:account/type (first group)) account-type-caption-map)
+                          :value total
+                          :style :header
+                          :depth 0}
+                  formatted-group (format-accounts group)]
+              (apply vector (concat (conj result header) formatted-group))))
           []
           grouped-accounts))
