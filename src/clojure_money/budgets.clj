f4312339190afbdc3d5c4ed25e5bc18d1919ff6b
Doug Knight <dougbknight@gmail.com>
2015-03-05 14:36:45 -0600

    add an item to a budget

--- src/clojure_money/budgets.clj
+++ src/clojure_money/budgets.clj
@@ -1,6 +1,7 @@
 (ns clojure-money.budgets
   (:require [datomic.api :as d :refer [transact q db]]
-            [clojure-money.common :as m :refer :all])
+            [clojure-money.common :as m :refer :all]
+            [clojure-money.accounts :refer :all])
   (:gen-class))
 
 (defn all-budgets
@@ -20,3 +21,36 @@
                  :budget/name budget-name
                  :budget/start-date start-date}]
     @(d/transact conn [tx-data])))
+
+(defn find-budget-by-name
+  "Returns the budget having the specified name, or nil if no such budget was found"
+  [db budget-name]
+  (->> (d/q
+         '[:find ?b
+           :in $ ?budget-name
+           :where [?b :budget/name ?budget-name]],
+         db,
+         budget-name)
+       first
+       (m/hydrate-entity db)))
+
+(defn add-budget-item
+  "Adds a line item to a budget"
+  [conn budget-or-name account-or-path amounts]
+  (let [budget (if (string? budget-or-name)
+                 (find-budget-by-name (d/db conn) budget-or-name)
+                 budget-or-name)
+        account (if (string? account-or-path)
+                  (find-account-by-path (d/db conn) account-or-path)
+                  account-or-path)
+        periods (map-indexed
+                  #(hash-map :budget-item-period/index %1 :budget-item-period/amount %2)
+                  amounts)
+        tx-data [{:db/id (:db/id budget)
+                  :budget/items [{:budget-item/account (:db/id account)
+                                  :budget-item/periods periods}]}]]
+
+    (if-not budget (throw (IllegalArgumentException. (str "Unable to find a budget named \"" budget-or-name "\""))))
+    (if-not account (throw (IllegalArgumentException. (str "Unable to find an account with the path \"" account-or-path "\""))))
+
+    @(d/transact conn tx-data)))
--- test/clojure_money/budgets_test.clj
+++ test/clojure_money/budgets_test.clj
@@ -1,11 +1,61 @@
 (ns clojure-money.budgets-test
   (:require [expectations :refer :all]
-            [datomic.api :as d :refer [db]]
-            [clojure-money.budgets :refer :all])
+            [datomic.api :as d :refer [db touch]]
+            [clojure-money.budgets :refer :all]
+            [clojure-money.accounts :refer :all]
+            [clojure-money.common :refer :all])
   (:use clojure-money.test-common))
+
+(defn prepare-db
+  "Prepares the database for budget tests"
+  []
+  (let [conn (create-empty-db)]
+    (add-account conn "Salary" :account.type/income)
+    (add-account conn "Groceries" :account.type/expense)
+    (add-account conn "Checking" :account.type/asset)
+    conn))
 
 ;; When I add a budget, it should appear in the list of budgets
 (expect [{:budget/name "2015" :budget/start-date #inst "2015-01-01"}]
         (let [conn (create-empty-db)]
           (add-budget conn "2015" #inst "2015-01-01")
           (map #(into {} %) (all-budgets (d/db conn)))))
+
+;; When I add a budget, I should be able to find it by name
+(expect {:budget/name "2016" :budget/start-date #inst "2016-01-01"}
+        (let [conn (create-empty-db)]
+          (add-budget conn "2016" #inst "2016-01-01")
+          (into {} (find-budget-by-name (d/db conn) "2016"))))
+
+;; When I add an item to a budget, it should appear in the budget items attribute
+(expect [300M 300M 300M 300M 300M 300M 300M 300M 300M 300M 300M 300M]
+        (let [conn (prepare-db)]
+          (add-budget conn "2015" #inst "2015-01-01")
+          (add-budget-item conn "2015" "Groceries" (repeat 12 (bigdec 300)))
+          (let [first-item (-> conn
+                               d/db
+                               (find-budget-by-name "2015")
+                               :budget/items
+                               first)]
+            (->> first-item
+                 :budget-item/periods
+                 (sort-by :budget-item-period/index)
+                 (map :budget-item-period/amount)))))
+; I couldn't figure out how to test both of these in one expectation
+(expect "Groceries"
+        (let [conn (prepare-db)]
+          (add-budget conn "2015" #inst "2015-01-01")
+          (add-budget-item conn "2015" "Groceries" (repeat 12 (bigdec 300)))
+          (let [first-item (-> conn
+                               d/db
+                               (find-budget-by-name "2015")
+                               :budget/items
+                               first)]
+            (->> first-item
+                 :budget-item/account
+                 d/touch
+                 :account/name))))
+
+;; TODO Add by explicit monthly values, monthly average, annual total
+
+;; Each account should only be listed one time for a given budget
