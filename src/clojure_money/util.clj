cafeb4083497913d22f428febcb092e9e7c5dcce
Doug Knight <dougbknight@gmail.com>
2015-03-20 16:42:46 -0500

    added util file for utility functions

---
+++ src/clojure_money/util.clj
@@ -0,0 +1,12 @@
+(ns clojure-money.util)
+
+(defn println-and-return
+  "Prints the specified information and then returns it"
+  ([caption to-print] (println-and-return caption identity to-print))
+  ([caption transform-fn to-print]
+   (println (str "*** " caption " ***"))
+   (if (coll? to-print)
+     (doseq [p to-print]
+       (println (str "  " (transform-fn p))))
+     (println (str "  " (transform-fn to-print))))
+   (println (str "*** end - " caption " ***"))))
