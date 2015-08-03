5274746567baa2242473f2f3daf210644e619755
Doug Knight <dougbknight@gmail.com>
2015-08-03 13:05:41 -0500

    add jetty dependency and create a simple handler

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
 (defproject clojure-money "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Application implementing double-entry accounting"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.datomic/datomic-free "0.9.5067" :exclusions [joda-time]]
-                 [clj-time "0.9.0"]]
+                 [clj-time "0.9.0"]
+                 [ring "1.4.0"]]
   :main ^:skip-aot clojure-money.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
--- src/clojure_money/core.clj
+++ src/clojure_money/core.clj
@@ -1,25 +1,12 @@
 (ns clojure-money.core
-  (:require [datomic.api :as d :refer [create-database connect transact q]]
-            [clojure-money.accounts :refer :all])
-  (:use clojure-money.common)
-  (:gen-class))
+  (:require [ring.adapter.jetty :as jetty]))
+
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "<html><body><h1>Hello!</h1>It's working!</body></html>"})
 
 (defn -main
   "main entry point for the application"
   [& args]
-  (when (= "init-database" (first args))
-    (init-database))
-  (when (= "add-account" (first args))
-    (let [conn (d/connect uri)]
-      (apply add-account (concat [conn] (rest args)))))
-  (when (= "all-accounts" (first args))
-
-    (println "all-accounts")
-
-    (let [db (d/db (d/connect uri))
-          accounts (all-accounts db)]
-
-      (println (str "found " (count accounts) " account(s)"))
-
-      (doseq [a accounts]
-        (println (str (:account/name a) ": " (:account/type a) ": " (:account/balance a)))))))
+  (jetty/run-jetty handler {:port 3000}))
