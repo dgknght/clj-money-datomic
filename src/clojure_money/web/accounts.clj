b1c67f6466a41d8bad2729a9928228dc24baee9f
Doug Knight <dougbknight@gmail.com>
2015-08-11 16:38:08 -0500

    add bootstrap files

--- src/clojure_money/web/accounts.clj
+++ src/clojure_money/web/accounts.clj
@@ -10,6 +10,12 @@
                [:head
                 [:meta {:http-equiv "Content-Type"
                         :content "text/html; charset=utf-8;"}]
+
+                ; Bootstrap
+                [:link {:rel "stylesheet" :href "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"}]
+                [:link {:rel "stylesheet" :href "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"}]
+                [:script {:src "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"}]
+
                 [:title (str "clj-money - " title)]]
                [:body content])))
 
