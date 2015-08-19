f39c6dc649a5595262e50e9d15d928196acdfcff
Doug Knight <dougbknight@gmail.com>
2015-08-19 14:40:00 -0500

    use the configured URI

--- src/clojure_money/web/accounts.clj
+++ src/clojure_money/web/accounts.clj
@@ -1,19 +1,18 @@
 (ns clojure-money.web.accounts
   (:require [clojure-money.accounts :as accounts]
+            [clojure-money.common :as common]
             [clojure-money.web.layouts :refer :all]
             [datomic.api :as d]
             [hiccup.core :refer :all]
             [hiccup.page :refer :all]))
 
-; TODO read this from the settings
-(def uri "datomic:free://localhost:4334/money")
 
 (defn index-accounts []
   (main-layout
     "Accounts"
     [:div {:class "page-header"}
      [:h1 "Accounts"]]
-    (let [conn (d/connect uri)]
+    (let [conn (d/connect common/uri)]
       [:table
        [:tr
         [:th "Name"]]
