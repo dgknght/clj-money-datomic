a8ba01fb37283530f500eba0e825c1ec28f71a9c
Doug Knight <dougbknight@gmail.com>
2015-08-19 08:55:50 -0500

    add list of accounts to the account index page

--- src/clojure_money/web/accounts.clj
+++ src/clojure_money/web/accounts.clj
@@ -1,12 +1,21 @@
 (ns clojure-money.web.accounts
   (:require [clojure-money.accounts :as accounts]
             [clojure-money.web.layouts :refer :all]
+            [datomic.api :as d]
             [hiccup.core :refer :all]
             [hiccup.page :refer :all]))
+
+; TODO read this from the settings
+(def uri "datomic:free://localhost:4334/money")
 
 (defn index-accounts []
   (main-layout
     "Accounts"
     [:div {:class "page-header"}
      [:h1 "Accounts"]]
-    [:p  "Coming soon."]))
+    (let [conn (d/connect uri)]
+      [:table
+       [:tr
+        [:th "Name"]]
+       (doseq [account (accounts/all-accounts (d/db conn))]
+         [:td (:name account)])])))
