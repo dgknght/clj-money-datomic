5c1539837def7254862f5a8c56c6da1371050991
Doug Knight <dougbknight@gmail.com>
2015-08-20 12:27:29 -0500

    rework the web application definition for better starting and stopping, middleware

--- src/clojure_money/core.clj
+++ src/clojure_money/core.clj
@@ -1,19 +1,22 @@
 (ns clojure-money.core
   (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.defaults :refer :all]
             [compojure.core :refer :all]
             [compojure.route :as route]
             [hiccup.core :refer :all]
             [clojure-money.web.accounts :as accounts]))
 
-(defroutes app
+(defroutes app-routes
   (GET "/accounts" [] (accounts/index-accounts))
+  (POST "/accounts" req (accounts/create-account (:params req)))
   (GET "/accounts/new" [] (accounts/new-account))
   (route/not-found (html [:h1 "Resource not found"])))
+
+(def app (wrap-defaults app-routes site-defaults))
+
+(defonce server (jetty/run-jetty #'app {:port 3000 :join? false}))
 
 (defn -main
   "main entry point for the application"
   [& args]
-  (jetty/run-jetty app {:port 3000}))
-
-(defn go! []
-  (jetty/run-jetty app {:port 3000}))
+  (.start server))
