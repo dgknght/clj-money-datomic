6d9bb6442162a9ab500f1e781b2553f8a3f71ef7
Doug Knight <dougbknight@gmail.com>
2015-08-20 10:30:23 -0500

    refine new account form
    use short syntax for classes
    add parent
    use different page header and title for each view

--- src/clojure_money/web/accounts.clj
+++ src/clojure_money/web/accounts.clj
@@ -6,31 +6,33 @@
             [hiccup.core :refer :all]
             [hiccup.page :refer :all]))
 
-
-(defn accounts-layout
-  "Layout for all account activities"
-  [& content]
+(defn index-accounts []
   (main-layout
     "Accounts"
-    [:div {:class "page-header"}
+    [:div.page-header
      [:h1 "Accounts"]]
-    content))
-
-(defn index-accounts []
-  (accounts-layout
     (let [conn (d/connect common/uri)]
-      [:table
+      [:table.table
        [:tr
         [:th "Name"]]
        (doseq [account (accounts/all-accounts (d/db conn))]
-         [:td (:name account)])])))
+         [:td (:name account)])])
+    [:a.btn.btn-default {:href "accounts/new"} "New"]))
 
 (defn new-account
   "Renders a form that can be submitted to create a new account"
   []
-  (accounts-layout
+  (main-layout
+    "New Account"
+    [:div.page-header
+     [:h1 "New Account"]]
     [:form {:role "form" :action "/accounts" :method "POST"}
-     [:div {:class "form-group"}
+     [:div.form-group
       [:label {:for "name"} "Name"]
-      [:text-field {:id "name" :name "name" :class "form-control"}]]
-     [:button {:type "submit" :class "btn btn-default"} "Save"]]))
+      [:text-field.form-control {:id "name" :name "name" :autofocus 1}]]
+     [:div.form-group
+      [:label {:for "parent"}]
+      [:select.form-control
+       [:option {:value 1} "Other account"]
+       [:option {:value 2} "Different account"]]]
+     [:button.btn.btn-default {:type "submit"} "Save"]]))
