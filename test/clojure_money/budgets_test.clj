f1b13649021f1e5b9fb40d5d5e2b0ff0ef978830
Doug Knight <dougbknight@gmail.com>
2015-08-25 15:09:38 -0500

    use clojure.test instead of expectations for budgets

--- test/clojure_money/budgets_test.clj
+++ test/clojure_money/budgets_test.clj
@@ -1,5 +1,5 @@
 (ns clojure-money.budgets-test
-  (:require [expectations :refer :all]
+  (:require [clojure.test :refer :all]
             [datomic.api :as d :refer [db touch]]
             [clojure-money.budgets :refer :all]
             [clojure-money.accounts :refer :all]
@@ -15,106 +15,95 @@
     (add-account conn "Checking" :account.type/asset)
     conn))
 
-;; When I add a budget, it should appear in the list of budgets
-(expect [{:budget/name "2015" :budget/start-date #inst "2015-01-01"}]
-        (let [conn (create-empty-db)]
-          (add-budget conn "2015" #inst "2015-01-01")
-          (map #(into {} %) (all-budgets (d/db conn)))))
+(deftest add-a-budget
+  (testing "When I add a budget, it appears in the list of budgets"
+    (let [conn (create-empty-db)
+          _ (add-budget conn "2015" #inst "2015-01-01")
+          budgets (map #(into {} %) (all-budgets (d/db conn)))]
+      (is (= budgets [{:budget/name "2015" :budget/start-date #inst "2015-01-01"}]))))
+  (testing "When I add a budget, I can find it by name"
+    (let [conn (create-empty-db)
+          _ (add-budget conn "2016" #inst "2016-01-01")
+          budget (into {} (find-budget-by-name (d/db conn) "2016"))]
+      (is (= budget {:budget/name "2016" :budget/start-date #inst "2016-01-01"})))))
 
-;; When I add a budget, I should be able to find it by name
-(expect {:budget/name "2016" :budget/start-date #inst "2016-01-01"}
-        (let [conn (create-empty-db)]
-          (add-budget conn "2016" #inst "2016-01-01")
-          (into {} (find-budget-by-name (d/db conn) "2016"))))
+(deftest add-a-budget-item
+  (testing "When I add an item to a budget, it should appear in the budget items attribute"
+    (let [conn (prepare-db)
+          _ (add-budget conn "2015" #inst "2015-01-01")
+          _ (add-budget-item conn "2015" "Groceries" (repeat 12 (bigdec 300)))
+          first-item (-> conn
+                         d/db
+                         (find-budget-by-name "2015")
+                         :budget/items
+                         first)
+          account-name (->> first-item
+                            :budget-item/account
+                            d/touch
+                            :account/name)
+          period-amounts (->> first-item
+                              :budget-item/periods
+                              (sort-by :budget-item-period/index)
+                              (map :budget-item-period/amount))]
+      (is (= period-amounts [300M 300M 300M 300M 300M 300M 300M 300M 300M 300M 300M 300M]))
+      (is (= account-name "Groceries"))))
+  (testing "If I add a budget item for an account that already has a budget item, a new item is not created"
+    (let [conn (prepare-db)
+          _ (add-budget conn "2015" #inst "2015-01-01")
+          _ (add-budget-item conn "2015" "Groceries" (repeat 12 (bigdec 100)))
+          _ (add-budget-item conn "2015" "Groceries" (repeat 12 (bigdec 300)))
+          item-count (-> conn
+                         d/db
+                         (find-budget-by-name "2015")
+                         :budget/items
+                         count)]
+      (is (= 1 item-count))))
+  (testing "If I add a budget item for an account that already has a budget item, the budget values are overwritten"
+    (let [conn (prepare-db)
+          _ (add-budget conn "2015" #inst "2015-01-01")
+          _ (add-budget-item conn "2015" "Groceries" (repeat 12 (bigdec 100)))
+          _ (add-budget-item conn "2015" "Groceries" (repeat 12 (bigdec 300)))
+          first-item (-> conn
+                         d/db
+                         (find-budget-by-name "2015")
+                         :budget/items
+                         first)
+          amounts (->> first-item
+                       :budget-item/periods
+                       (sort-by :budget-item-period/index)
+                       (map :budget-item-period/amount))]
+      (is (= amounts [300M 300M 300M 300M 300M 300M 300M 300M 300M 300M 300M 300M])))))
 
-;; When I add an item to a budget, it should appear in the budget items attribute
-(expect [300M 300M 300M 300M 300M 300M 300M 300M 300M 300M 300M 300M]
-        (let [conn (prepare-db)]
-          (add-budget conn "2015" #inst "2015-01-01")
-          (add-budget-item conn "2015" "Groceries" (repeat 12 (bigdec 300)))
-          (let [first-item (-> conn
-                               d/db
-                               (find-budget-by-name "2015")
-                               :budget/items
-                               first)]
-            (->> first-item
-                 :budget-item/periods
-                 (sort-by :budget-item-period/index)
-                 (map :budget-item-period/amount)))))
-; I couldn't figure out how to test both of these in one expectation
-(expect "Groceries"
-        (let [conn (prepare-db)]
-          (add-budget conn "2015" #inst "2015-01-01")
-          (add-budget-item conn "2015" "Groceries" (repeat 12 (bigdec 300)))
-          (let [first-item (-> conn
-                               d/db
-                               (find-budget-by-name "2015")
-                               :budget/items
-                               first)]
-            (->> first-item
-                 :budget-item/account
-                 d/touch
-                 :account/name))))
+(deftest get-budget-end-date
+  (testing "Given a budget, I can get the end date"
+    (let [conn (prepare-db)
+          _ (add-budget conn "2015" #inst "2015-01-01")
+          end-date (budget-end-date (find-budget-by-name (d/db conn) "2015"))]
+      (is (= end-date #inst "2015-12-31")))))
 
-;; If I add a budget item for an account that already has a budget item,
-;; a new budget item is not created
-(expect 1
-        (let [conn (prepare-db)]
-          (add-budget conn "2015" #inst "2015-01-01")
-          (add-budget-item conn "2015" "Groceries" (repeat 12 (bigdec 100)))
-          (add-budget-item conn "2015" "Groceries" (repeat 12 (bigdec 300)))
-          (-> conn
-              d/db
-              (find-budget-by-name "2015")
-              :budget/items
-              count)))
+(deftest get-a-budget-amount
+  (testing "Given a budget, and account, and a number of periods, I can get a budget amount"
+    (let [conn (prepare-db)
+          _ (add-budget conn "2015" #inst "2015-01-01")
+          _ (add-budget-item conn "2015" "Groceries" (repeat 12 (bigdec 100)))
+          budget-amount (get-budget-amount (d/db conn) "2015" "Groceries" 3)]
+      (is (= (bigdec 300) budget-amount)))))
 
-;; If I add a budget item for an account that already has a budget item,
-;; the specified values overwrite the existing values
-(expect [300M 300M 300M 300M 300M 300M 300M 300M 300M 300M 300M 300M]
-        (let [conn (prepare-db)]
-          (add-budget conn "2015" #inst "2015-01-01")
-          (add-budget-item conn "2015" "Groceries" (repeat 12 (bigdec 100)))
-          (add-budget-item conn "2015" "Groceries" (repeat 12 (bigdec 300)))
-          (let [first-item (-> conn
-                               d/db
-                               (find-budget-by-name "2015")
-                               :budget/items
-                               first)]
-            (->> first-item
-                 :budget-item/periods
-                 (sort-by :budget-item-period/index)
-                 (map :budget-item-period/amount)))))
+(deftest get-a-budget-from-a-date
+  (testing "Given a date, I can get the budget that includes that date"
+    (let [conn (prepare-db)
+          _ (add-budget conn "2016" #inst "2016-01-01")
+          _ (add-budget conn "2015" #inst "2015-01-01")
+          _ (add-budget conn "2014" #inst "2014-01-01")
+          budget-name (find-budget-containing-date (d/db conn) #inst "2015-02-27")]
+      (is (= "2015" (:budget/name budget-name))))))
 
-;; Given a budget, I should be able to get the end date
-(expect #inst "2015-12-31"
-        (let [conn (prepare-db)]
-          (add-budget conn "2015" #inst "2015-01-01")
-          (budget-end-date (find-budget-by-name (d/db conn) "2015"))))
-
-;; Given a budget, an account, and a number of periods, I should be able to get the budget amount
-(expect (bigdec 300)
-        (let [conn (prepare-db)]
-          (add-budget conn "2015" #inst "2015-01-01")
-          (add-budget-item conn "2015" "Groceries" (repeat 12 (bigdec 100)))
-          (get-budget-amount (d/db conn) "2015" "Groceries" 3)))
-
-;; Given a date, I should be able to retrieve the budget that includes that date
-(expect "2015"
-        (let [conn (prepare-db)]
-          (add-budget conn "2016" #inst "2016-01-01")
-          (add-budget conn "2015" #inst "2015-01-01")
-          (add-budget conn "2014" #inst "2014-01-01")
-          (-> (find-budget-containing-date (d/db conn) #inst "2015-02-27")
-              :budget/name)))
-
-;; Given a budget item, I should be able to retrieve the period
-;; that contains the specified date
-(expect (bigdec 300)
-        (let [conn (prepare-db)]
-          (add-budget conn "2015" #inst "2015-01-01")
-          (add-budget-item conn "2015" "Groceries" (take 12 (iterate (partial + (bigdec 100)) (bigdec 100))))
-          (-> conn
-              d/db
-              (find-budget-item-period "2015" "Groceries" #inst "2015-03-02")
-              :budget-item-period/amount)))
+(deftest get-a-budget-item-period
+  (testing "Given a budget item, I can get the budget period"
+    (let [conn (prepare-db)
+          _ (add-budget conn "2015" #inst "2015-01-01")
+          _ (add-budget-item conn "2015" "Groceries" (take 12 (iterate (partial + (bigdec 100)) (bigdec 100))))
+          budget-item-period (-> conn
+                                 d/db
+                                 (find-budget-item-period "2015" "Groceries" #inst "2015-03-02"))]
+      (is (= (bigdec 300) (:budget-item-period/amount budget-item-period))))))
