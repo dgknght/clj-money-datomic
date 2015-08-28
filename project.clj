0c8674f16f80676049396d71603490584046efa3
Doug Knight <dougbknight@gmail.com>
2015-08-28 16:49:51 -0500

    adjust dependencies so that logging works

--- project.clj
+++ project.clj
@@ -4,8 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/tools.logging "0.3.1"]
-                 [com.datomic/datomic-free "0.9.5067" :exclusions [joda-time]]
+                 [org.clojure/tools.logging "0.3.1" :exclusions [javax.mail/mail
+                                                                 javax.jms/jms
+                                                                 com.sun.jdmk/jmxtools
+                                                                 com.sun.jmx/jmxri]]
+                 [com.datomic/datomic-free "0.9.5067" :exclusions [joda-time
+                                                                   org.slf4j/slf4j-nop
+                                                                   org.slf4j/slj4j-log4j12]]
+                 [ch.qos.logback/logback-classic "1.1.3"]
                  [clj-time "0.9.0"]
                  [ring "1.4.0"]
                  [ring/ring-defaults "0.1.5"]
