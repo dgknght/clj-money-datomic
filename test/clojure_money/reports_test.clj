e5736658fc0db11b3b06fade0303ec09f3a79f48
Doug Knight <dougbknight@gmail.com>
2015-09-03 17:06:35 -0500

    clean up report test setup

--- test/clojure_money/reports_test.clj
+++ test/clojure_money/reports_test.clj
@@ -11,16 +11,16 @@
   "Creates and populates a database"
   []
   (let [conn (create-empty-db)]
-    (add-account conn {:account/name "Checking"         :account/type :account.type/asset})
-    (add-account conn {:account/name "Savings"          :account/type :account.type/asset})
-    (add-account conn {:account/name "Car"              :account/type :account.type/asset :account/parent "Savings"})
-    (add-account conn {:account/name "Reserve"          :account/type :account.type/asset :account/parent "Savings"})
-    (add-account conn {:account/name "Credit card"      :account/type :account.type/liability})
-    (add-account conn {:account/name "Opening balances" :account/type :account.type/equity})
-    (add-account conn {:account/name "Salary"           :account/type :account.type/income})
-    (add-account conn {:account/name "Groceries"        :account/type :account.type/expense})
-    (add-account conn {:account/name "Food"             :account/type :account.type/expense :account/parent "Groceries"})
-    (add-account conn {:account/name "Non-food"         :account/type :account.type/expense :account/parent "Groceries"})
+    (add-accounts conn [{:account/name "Checking"         :account/type :account.type/asset}
+                        {:account/name "Savings"          :account/type :account.type/asset}
+                        {:account/name "Car"              :account/type :account.type/asset :account/parent "Savings"}
+                        {:account/name "Reserve"          :account/type :account.type/asset :account/parent "Savings"}
+                        {:account/name "Credit card"      :account/type :account.type/liability}
+                        {:account/name "Opening balances" :account/type :account.type/equity}
+                        {:account/name "Salary"           :account/type :account.type/income}
+                        {:account/name "Groceries"        :account/type :account.type/expense}
+                        {:account/name "Food"             :account/type :account.type/expense :account/parent "Groceries"}
+                        {:account/name "Non-food"         :account/type :account.type/expense :account/parent "Groceries"}])
 
     (add-budget conn "2015" #inst "2015-01-01")
     (add-budget-item conn "2015" "Salary"             (repeat 12 (bigdec 1800)))
