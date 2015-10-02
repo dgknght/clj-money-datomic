a86830b1018b19383dfb87ffea45944472e60d8b
Doug Knight <dougbknight@gmail.com>
2015-10-02 17:43:09 -0500

    add test for two transaction items on the same date

--- test/clj_money/transactions_test.clj
+++ test/clj_money/transactions_test.clj
@@ -446,4 +446,33 @@
                                (select-keys t [:transaction/date])))
                       items)]
       (is (= expected
+             actual))))
+  (testing "A new item is inserted before other items with the same date"
+    (let [conn (new-test-db)
+          _ (add-simple-transaction conn {:transaction/date #inst "2015-09-01"
+                                          :transaction/description "Kroger"
+                                          :amount (bigdec 100)
+                                          :debit-account "Groceries"
+                                          :credit-account "Checking"})
+          _ (add-simple-transaction conn {:transaction/date #inst "2015-09-01"
+                                          :transaction/description "Paycheck"
+                                          :amount (bigdec 1000)
+                                          :debit-account "Checking"
+                                          :credit-account "Salary"})
+          account (resolve-account (d/db conn) "Checking")
+          items (get-account-transaction-items (d/db conn) (:db/id account))
+          expected [{:transaction-item/index 1
+                     :transaction/date #inst "2015-09-01"
+                     :transaction-item/balance (bigdec 900)}
+                    {:transaction-item/index 0
+                     :transaction/date #inst "2015-09-01"
+                     :transaction-item/balance (bigdec 1000)}]
+          actual (map (fn [[i t]]
+                        (merge (select-keys i [:transaction-item/index :transaction-item/balance])
+                               (select-keys t [:transaction/date])))
+                      items)]
+
+      (pprint-diff expected actual)
+
+      (is (= expected
              actual)))))
